2acf04e9596379420b3b0dc9056a106c21fa899d
Okwudili David <michealdavid910@gmail.com>
2020-06-06 14:17:20 +0100

    added: initial commit

---
+++ GitTesting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Started...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GitTesting/Test_1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTesting
+{
+    public class Test_1
+    {
+        public string Id { get; set; }
+        public string Id1 { get; set; }
+        public string Id2 { get; set; }
+        public string Id3 { get; set; }
+        public string Id4 { get; set; }
+        public string Id5 { get; set; }
+        public string Id6 { get; set; }
+        public string Id7 { get; set; }
+        public string Id8 { get; set; }
+    }
+}
